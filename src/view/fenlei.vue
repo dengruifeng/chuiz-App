b03c4ce6af7af67155624d3112d91c7448f9b5f3
“dengruifeng <976987127@qq.com>
2019-04-15 10:53:58 +0800

    写了一点

--- src/view/fenlei.vue
+++ src/view/fenlei.vue
@@ -1,34 +1,17 @@
 <template>
     <div class="fenlei">
         <navhead></navhead>
-        <div class="fenlei-list">
-            <ul>
-              <li>新品</li>
-              <li>手机</li>
-              <li>总裁</li>
-            </ul>
+        <div class="rui-phone">
+            <h2>手机</h2>
         </div>
-        <div class="shopping-list">
-            <div>
-                <div>
-                    <a href="">
-                        <img src="../style/img/kk500x200.webp" alt="" srcset="">
-                    </a>
-                </div>
-                <div>
-                   <span>手机</span>
-                </div>
-                <div>
-                    <div>
-                        <img src="../style/img/m9.png" alt="" srcset="">
-                        小米9
-                    </div>
-                    <div></div>
-                    <div></div>
-                    
-                </div>
-            </div>
-
+        <div class="rui-ban">
+            <a href=""><img src="../style/img/rui-ban.webp" alt="手机"></a>
+        </div>
+        <div class="rui-sj">
+            <a href=""></a>
+            <img src="../style/img/rui-sj.webp">
+            <span>坚果 Pro 2S</span>
+            <i class="iconfont icon-jiantou"></i>
         </div>
     </div>
 </template>
@@ -61,104 +44,40 @@ components:{
 
 <style lang="less">
 .fenlei{
-    position: relative;
-    margin: 0 auto;
-    .fenlei-list{
-        position: fixed;
-        top: 50px;
-        bottom: 52px;
-        left: 0;
-        width: 85px;
-        border-right: 1px solid #efefef;
-        overflow: hidden;
-        ul{
-            z-index: 90;
-            position: absolute;
-            top: 0;
-            bottom: 0;
-            left: 0;
-            padding: 12px 0 12px 0;
-            background: #fefefe;
-            overflow-y: auto;
-            width: 100%;
-            li{
-                font-size: 14px;
-                width: 63px;
-                height: 38px;
-                line-height: 38px;
-                overflow: hidden;
-                text-align: center;
-                margin: 0 auto;
-            }
+    .rui-phone{
+        height: 40px;
+        width: 100%;
+        margin-top: 50px;
+        line-height: 40px;
+        h2{
+            font-size: 18px;
+            font-weight: 700;
+            padding-left: 10px;
         }
     }
-    .shopping-list{
-        padding-left: 102px;
-        padding-right: 17px;
-        padding-top: 50px;
-        margin: 0 auto;
-        div:nth-child(1){
-            a{
-                display: block;
-                width: 280px;
-                height: 110px;
-                img{
-                    display: block;
-                    width: 100%;
-                    height: 100%;
-                }
-            }
+    .rui-ban{
+        width: 100%;
+        height: 132px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-top: 1px solid  #ebebeb;
+        img{
+            display: block;
+            width: 387px;
+            height: 99px;
         }
-        div:nth-child(2){
-            width: 280px;
-            height: 71px;
-            line-height: 71px;
-            background: #fff;
-            font-size: 14px;
-            text-align: center;
-            margin-top: 2px;
-            overflow: hidden;
-            
-            span{
-                position: relative;
-            }
-            span:before{
-                content: "";
-                position: absolute;
-                top: 50%;
-                left: 0;
-                width: 24px;
-                border-top: 1px solid #e0e0e0;
-                transform: translate3d(-150%,-50%,0);
-                -webkit-transform: translate3d(-150%,-50%,0);
-            }
-            span:after{
-                content: "";
-                left: auto;
-                right: 0;
-                position: absolute;
-                top: 50%;
-                width: 24px;
-                border-top: 1px solid #e0e0e0;
-                transform: translate3d(150%,-50%,0);
-                -webkit-transform: translate3d(150%,-50%,0);
-            }
-        }
-        div:nth-child(3){
-             width: 280px;
-             display: flex;
-             justify-content: space-between;
-             text-align: center;
-             flex-direction: column;
-             div{
-                 width: 25%;
-                img{
-                    display: block;
-                    width: 56px;
-                    margin: 0 auto;
-                    margin-bottom: 15px;
-                }
-             }
+    }
+    .rui-sj{
+        width: 100%;
+        height: 69px;
+        line-height: 69px;
+        text-align: left;
+        img{
+            display: inline-block;
+            width: 41px;
+            height: 41px;
+            flex: 0 0 auto;
         }
     }
 }
