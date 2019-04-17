6c2f92b6cca02fb506d367fe7ecde8630f27963f
wsj5846 <1831371976@qq.com>
2019-04-17 21:03:02 +0800

    search完成

--- src/router.js
+++ src/router.js
@@ -49,7 +49,7 @@ const router = new VueRouter({
         path: '/login',
         component: () => import('./view/user/login.vue'),
         beforeEnter: (to, from, next) => {
-            console.log('路由独享的守卫， 只会在要进入login的时候触发')
+            //console.log('路由独享的守卫， 只会在要进入login的时候触发')
             next()
         }
     },
@@ -79,13 +79,6 @@ const router = new VueRouter({
             }
         }
     },
-<<<<<<< HEAD
-    {path:'/login',component: ()=> import('./view/login.vue')},
-    {path:'/register',component: ()=> import('./view/register.vue')},
-    {path:'/details',component: ()=> import('./view/xiangqing.vue')},
-    {path:'/search',component: ()=> import('./view/search.vue')}
-=======
->>>>>>> f87baa5cc237e0f31223b89cc5e175496c989374
     ]
 })
 
@@ -93,13 +86,10 @@ router.beforeEach((to, from, next) => {
     //to 将要去的路由对象
     // from 当前的路由对象
     // next 是否允许 如果使用就不跳转
-    console.log('全局前置守卫')
-    console.log(to)
-    console.log(from)
+    //console.log('全局前置守卫')
+    //console.log(to)
+    //console.log(from)
     next();
-})
-router.afterEach((to, from) => {
-    console.log('全局后置守卫')
 })
 
 export default router;
